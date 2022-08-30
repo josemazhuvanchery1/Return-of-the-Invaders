7173679c76ad289afacfaa8b48450e4ea98e4525
Celybot <youancece@gmail.com>
2022-08-30 15:33:34 -0400

    added: new html as well as background sprite

---
+++ start_scene.js
@@ -0,0 +1,8 @@
+loadSprite('page1background', 'space-invaders-feature.avif')
+add([
+    sprite('page1background'),
+    pos(width()/2, height()/2 ),
+    origin("center"),
+    scale(1),
+    fixed()
+])
