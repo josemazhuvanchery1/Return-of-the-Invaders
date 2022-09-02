ea0ff104c1107a6b170f9293a996d6ce3b94ace4
josemazhuvanchery1 <jose1998mathew@gmail.com>
2022-09-01 20:19:58 -0400

    In the beginning there was nothing, and then there was the return of the invaders

---
+++ kaboom.js
@@ -0,0 +1,10 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+//initialize kaboom
+export const K = kaboom({
+    width: document.querySelector("body").width,
+    height:document.querySelector("body").height,
+    scale: 1
+})
+
+export default K;
