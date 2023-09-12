901a1e84a778551dca93bcea87727c6b5c474419
Celine Youance <101747839+Celybot@users.noreply.github.com>
2023-09-12 12:46:36 -0400

    Update kaboom.js

--- kaboom.js
+++ kaboom.js
@@ -1,5 +1,5 @@
 //import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
-import kaboom from "https://unpkg.com/kaboom@2000.2.10/dist/kaboom.js";
+import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.js";
 
 //initialize kaboom
 export const K = kaboom({
